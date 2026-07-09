ec72a91a42fb64a7588efbe8a7dafa6317e9adf3
ink-mami <inkman970@gmail.com>
2026-07-09 16:36:57 +0000

    feat(attester-registry): add storage schema and error types

--- contracts/attester-registry/src/lib.rs
+++ contracts/attester-registry/src/lib.rs
@@ -1,16 +1,28 @@
 #![no_std]
 
-use soroban_sdk::{contract, contractimpl, Env};
+use soroban_sdk::{contract, contracterror, contracttype, Address};
+
+/// Storage keys for the attester registry.
+#[contracttype]
+#[derive(Clone)]
+enum DataKey {
+    /// The address authorized to add/remove attesters.
+    Admin,
+    /// Presence of this key (mapped to `true`) means the address is an
+    /// allowlisted attester.
+    Attester(Address),
+}
+
+#[contracterror]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
+#[repr(u32)]
+pub enum Error {
+    NotInitialized = 1,
+    AlreadyInitialized = 2,
+}
 
 #[contract]
 pub struct AttesterRegistry;
-
-#[contractimpl]
-impl AttesterRegistry {
-    pub fn ping(_env: Env) -> bool {
-        true
-    }
-}
 
 #[cfg(test)]
 mod test;
--- contracts/attester-registry/src/test.rs
+++ contracts/attester-registry/src/test.rs
@@ -1,11 +1,3 @@
+#![cfg(test)]
+
 use super::*;
-use soroban_sdk::Env;
-
-#[test]
-fn ping_returns_true() {
-    let env = Env::default();
-    let contract_id = env.register(AttesterRegistry, ());
-    let client = AttesterRegistryClient::new(&env, &contract_id);
-
-    assert!(client.ping());
-}
